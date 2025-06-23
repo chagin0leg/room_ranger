e54925eb943ba31c13231d25b9a5b73c9e562ffb
chagin.0leg <chagin.0leg@yandex.ru>
2025-06-24 00:22:19 +0300

    refactor: move Google Calendar logic to separate GoogleCalendarService class and update usage in BookingContainer

--- lib/main.dart
+++ lib/main.dart
@@ -5,49 +5,7 @@ import 'package:room_ranger/utils/date_utils.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:googleapis/calendar/v3.dart' as calendar;
 import 'package:googleapis_auth/auth_io.dart';
-
-class CalendarService {
-  static const _scopes = [calendar.CalendarApi.calendarReadonlyScope];
-  static const _credentials = {
-    "type": "service_account",
-    "project_id": "YOUR_PROJECT_ID",
-    "private_key_id": "YOUR_PRIVATE_KEY_ID",
-    "private_key": "YOUR_PRIVATE_KEY",
-    "client_email": "YOUR_CLIENT_EMAIL",
-    "client_id": "YOUR_CLIENT_ID",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "YOUR_CERT_URL"
-  };
-
-  static Future<Set<DateTime>> getBookedDates() async {
-    final credentials = ServiceAccountCredentials.fromJson(_credentials);
-    final client = await clientViaServiceAccount(credentials, _scopes);
-    final calendarApi = calendar.CalendarApi(client);
-
-    final now = DateTime.now();
-    final startOfMonth = DateTime(now.year, now.month, 1);
-    final endOfMonth = DateTime(now.year, now.month + 1, 0);
-
-    final events = await calendarApi.events.list(
-      'primary',
-      timeMin: startOfMonth.toUtc(),
-      timeMax: endOfMonth.toUtc(),
-      singleEvents: true,
-      orderBy: 'startTime',
-    );
-
-    final bookedDates = <DateTime>{};
-    for (var event in events.items ?? []) {
-      if (event.start?.dateTime != null) {
-        bookedDates.add(event.start!.dateTime!.toLocal());
-      }
-    }
-
-    return bookedDates;
-  }
-}
+import 'package:room_ranger/utils/google_calendar_service.dart';
 
 class CalendarCell extends StatefulWidget {
   final int month;
@@ -254,26 +212,12 @@ class _BookingContainerState extends State<BookingContainer> {
   }
 
   Future<void> _loadBookedDates() async {
-    // Тестовые забронированные даты
-    final now = DateTime.now();
-    final testBookedDates = {
-      for (var i = 0; i < 64; i++)
-        DateTime(now.year, Random().nextInt(12) + 1, Random().nextInt(28) + 1),
-    };
-
-    setState(() {
-      _bookedDates = testBookedDates;
-    });
-
-    // Раскомментируйте для реальной интеграции с Google Calendar
-    // try {
-    //   final bookedDates = await CalendarService.getBookedDates();
-    //   setState(() {
-    //     _bookedDates = bookedDates;
-    //   });
-    // } catch (e) {
-    //   print('Error loading booked dates: $e');
-    // }
+    try {
+      final bookedDates = await GoogleCalendarService.getBookedDates();
+      setState(() => _bookedDates = bookedDates);
+    } catch (e) {
+      print('Error loading booked dates: $e');
+    }
   }
 
   void _onDateSelected(int day, int month) {
